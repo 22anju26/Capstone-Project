75b2ca0ecb324de677b6235b2f8f3d10a818ac38
22anju26 <82135608+22anju26@users.noreply.github.com>
2021-04-19 08:47:43 +0900

    Update andyu_pianotiles.py

--- andyu_pianotiles.py
+++ andyu_pianotiles.py
@@ -14,7 +14,144 @@ def open_splashscreen():
     # functions
     def play_game():
         root.destroy()
+        def game():
+            # turtle game window setup
+            wn = turtle.Screen()
+            wn.title("Piano Tiles")
+            wn.bgcolor("white")
+            wn.setup(410,800)
+            wn.tracer(0)
+            
+
+            # initial stuff
+            font = ("Calibri", 20, "bold")
+            possible_x = (-150, -50, 50, 150)
+            possible_y = (-300, -100, 100, 300)
+            q = -150, -300
+            w = -50, -300
+            e = 50, -300
+            r = 150, -300
+
+
+            # class
+            class Grid(turtle.Turtle):
+                def __init__(self, x, y):
+                    turtle.Turtle.__init__(self)
+                    self.penup()
+                    self.goto(x, y)
+                    self.shape("square")
+                    self.color("grey")
+                    self.speed(0)
+
+                def vertical(self):
+                    self.shapesize(400, 0.1, 0.1)
+                
+                def horizontal(self):
+                    self.shapesize(0.1, 400, 0.1)
+
+
+            class Tile(turtle.Turtle):
+                def __init__(self, y):
+                    turtle.Turtle.__init__(self)
+                    self.penup()
+                    self.goto(random.choice(possible_x), y)
+                    self.shape("square")
+                    self.shapesize(10, 5, 0.1)
+                    self.color("black")
+                    self.speed(0)
+
+
+            # scoreboard
+            scoreboard = turtle.Turtle()
+            scoreboard.hideturtle()
+            scoreboard.color("red")
+            scoreboard.speed(0)
+            scoreboard.penup()
+            scoreboard.goto(0, 350)
+            scoreboard.write(f"Score: {score} Lives: {int(lives)}", align = "center", font = font)
+
+
+            # grids
+            grid1 = Grid(-100, 0)
+            grid2 = Grid(0, 0)
+            grid3 = Grid(100, 0)
+            vertical_grids = (grid1, grid2, grid3)
+            for grid in vertical_grids:
+                grid.vertical()
+
+            grid4 = Grid(0, -200)
+            grid5 = Grid(0, 0)
+            grid6 = Grid(0, 200)
+            grid7 = Grid(0, -400)
+            horizontal_grids = (grid4, grid5, grid6, grid7)
+            for grid in horizontal_grids:
+                grid.horizontal()
+            
+            
+            # tiles
+            tile1 = Tile(-300)
+            tile2 = Tile(-100)
+            tile3 = Tile(100)
+            tile4 = Tile(300)
+            
+            tiles = [tile1, tile2, tile3, tile4]
+            
+            
+            # functions
+            def update_scoreboard():
+                scoreboard.clear()
+                scoreboard.write(f"Score: {score} Lives: {int(lives)}", align = "center", font = font)
+            
+            def move_down():
+                for tile in tiles:
+                    tile.sety(tile.ycor() - 200)
+            
+            def move(key):
+                global score
+                global lives
+                for tile in tiles:
+                    if tile.pos() == key:
+                        score += 10
+                        tile.goto(random.choice(possible_x), 300)
+                        tiles.remove(tile)
+                        move_down()
+                        tiles.append(tile)
+                        update_scoreboard()
+                        break
+                    else:
+                        lives -= 0.25
+                        game_over()
+                        update_scoreboard()
+                wn.update()
+            
+            
+            # keybinding
+            wn.listen()
+            wn.onkeypress((lambda:move(q)), "q")
+            wn.onkeypress((lambda:move(w)), "w")
+            wn.onkeypress((lambda:move(e)), "e")
+            wn.onkeypress((lambda:move(r)), "r")
+            
+            
+            # check life
+            def game_over():
+                global score
+                global lives
+                
+                if lives == 0.0:
+                    if score > pickle.load(open("highscore.dat", "rb")):
+                        pickle.dump(score, open("highscore.dat", "wb"))
+                    score = 0
+                    lives = 5
+                    wn.bye()
+                    open_splashscreen()
+            
+            
+            # mainloop
+            wn.update()
+            wn.mainloop()
         game()
+
 
     # widgets
     label_title = tk.Label(root, text = "Piano Tiles", font = ("Arial", 30))
@@ -23,7 +160,7 @@ def open_splashscreen():
     label_highscore = tk.Label(root, text = f"Highest Score: {pickle.load(open('highscore.dat', 'rb'))}", font = ("Arial", 25))
     label_highscore.pack()
     
-    button_play = tk.Button(root, text = "Play Game", command = play_game, width = 10)
+    button_play = tk.Button(root, text = "Play Game", command = play_game(), width = 10)
     button_play.pack()
     
     
@@ -31,142 +168,5 @@ def open_splashscreen():
 
 score = 0
 lives = 5
-
-def game():
-    # turtle game window setup
-    wn = turtle.Screen()
-    wn.title("Piano Tiles")
-    wn.bgcolor("white")
-    wn.setup(410,800)
-    wn.tracer(0)
-    
-
-    # initial stuff
-    font = ("Calibri", 20, "bold")
-    possible_x = (-150, -50, 50, 150)
-    possible_y = (-300, -100, 100, 300)
-    q = -150, -300
-    w = -50, -300
-    e = 50, -300
-    r = 150, -300
-
-
-    # class
-    class Grid(turtle.Turtle):
-        def __init__(self, x, y):
-            turtle.Turtle.__init__(self)
-            self.penup()
-            self.goto(x, y)
-            self.shape("square")
-            self.color("grey")
-            self.speed(0)
-
-        def vertical(self):
-            self.shapesize(400, 0.1, 0.1)
-        
-        def horizontal(self):
-            self.shapesize(0.1, 400, 0.1)
-
-
-    class Tile(turtle.Turtle):
-        def __init__(self, y):
-            turtle.Turtle.__init__(self)
-            self.penup()
-            self.goto(random.choice(possible_x), y)
-            self.shape("square")
-            self.shapesize(10, 5, 0.1)
-            self.color("black")
-            self.speed(0)
-
-
-    # scoreboard
-    scoreboard = turtle.Turtle()
-    scoreboard.hideturtle()
-    scoreboard.color("red")
-    scoreboard.speed(0)
-    scoreboard.penup()
-    scoreboard.goto(0, 350)
-    scoreboard.write(f"Score: {score} Lives: {int(lives)}", align = "center", font = font)
-
-
-    # grids
-    grid1 = Grid(-100, 0)
-    grid2 = Grid(0, 0)
-    grid3 = Grid(100, 0)
-    vertical_grids = (grid1, grid2, grid3)
-    for grid in vertical_grids:
-        grid.vertical()
-
-    grid4 = Grid(0, -200)
-    grid5 = Grid(0, 0)
-    grid6 = Grid(0, 200)
-    grid7 = Grid(0, -400)
-    horizontal_grids = (grid4, grid5, grid6, grid7)
-    for grid in horizontal_grids:
-        grid.horizontal()
-    
-    
-    # tiles
-    tile1 = Tile(-300)
-    tile2 = Tile(-100)
-    tile3 = Tile(100)
-    tile4 = Tile(300)
-    
-    tiles = [tile1, tile2, tile3, tile4]
-    
-    
-    # functions
-    def update_scoreboard():
-        scoreboard.clear()
-        scoreboard.write(f"Score: {score} Lives: {int(lives)}", align = "center", font = font)
-    
-    def move_down():
-        for tile in tiles:
-            tile.sety(tile.ycor() - 200)
-    
-    def move(key):
-        global score
-        global lives
-        for tile in tiles:
-            if tile.pos() == key:
-                score += 10
-                tile.goto(random.choice(possible_x), 300)
-                tiles.remove(tile)
-                move_down()
-                tiles.append(tile)
-                update_scoreboard()
-                break
-            else:
-                lives -= 0.25
-                game_over()
-                update_scoreboard()
-        wn.update()
-    
-    
-    # keybinding
-    wn.listen()
-    wn.onkeypress((lambda:move(q)), "q")
-    wn.onkeypress((lambda:move(w)), "w")
-    wn.onkeypress((lambda:move(e)), "e")
-    wn.onkeypress((lambda:move(r)), "r")
-    
-    
-    # check life
-    def game_over():
-        global score
-        global lives
-        
-        if lives == 0.0:
-            if score > pickle.load(open("highscore.dat", "rb")):
-                pickle.dump(score, open("highscore.dat", "wb"))
-            score = 0
-            lives = 5
-            wn.bye()
-            open_splashscreen()
-    
-    
-    # mainloop
-    wn.update()
-    wn.mainloop()
 
 open_splashscreen()
